dc74a43256be8b6792c28fa8767ea29f0c1f8f4b
Nihal Abdul Khader MP <nihalmp45@gmail.com>
2022-08-01 18:30:22 +0530

    initial version

---
+++ script.js
@@ -0,0 +1,15 @@
+function buttonclick(val)
+{
+    console.log(val)
+    document.getElementById("screen").value=document.getElementById("screen").value+val
+}
+function clearScreen()
+{
+    document.getElementById("screen").value=""
+}
+function equalClick()
+{
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
